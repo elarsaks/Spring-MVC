a4239e54ac81a5de9c981d662e1b413e93233f4b
elarsaks <elarsaks@gmail.com>
2021-08-07 09:58:07 +0300

    Spring Autowiring.

--- first-web-application/src/main/java/com/in28minutes/login/LoginService.java
+++ first-web-application/src/main/java/com/in28minutes/login/LoginService.java
@@ -1,11 +1,12 @@
 package com.in28minutes.login;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
-	
 	public boolean validateUser(String username, String password) {
 		if(username.equals("in28minutes") && password.equals("dummy"))
 			return  true;
 		return  false;
 	}
-
 }
